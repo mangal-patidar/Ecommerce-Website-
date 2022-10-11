a1f65171b61f5db68d10985c89b02ede2ff8e9dd
mangal patidar <mangal.patidar@zehntech.com>
2022-10-11 12:37:47 +0530

    adding item and some action in item_controller

---
+++ app/controllers/item_controller.rb
@@ -0,0 +1,40 @@
+class ItemController < ApplicationController
+    
+    def index
+        @item = Item.all
+    end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    def new
+        @item = Item.new
+    end
+
+    def create  
+        @item = Item.new(item_params)   
+        if @item.save   
+          flash[:notice] = 'Item added!'   
+          redirect_to root_path   
+        else   
+          flash[:error] = 'Failed to edit item!'   
+          render :new   
+        end   
+    end 
+
+    def destroy  
+        @item = Item.find(params[:id])   
+        if @item.update_attributes(item_params)   
+          flash[:notice] = 'Item updated!'   
+          redirect_to root_path   
+        else   
+          flash[:error] = 'Failed to edit item!'   
+          render :edit   
+        end   
+    end
+
+    def item_params   
+        params.require(:item).permit(:name, :price, :description)   
+    end 
+end
